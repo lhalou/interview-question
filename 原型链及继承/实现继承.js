fc1b2e8bb4eb3083c7b7c7fcee5f3a74877f695f
lhalou <614124284@qq.com>
2020-08-07 10:34:40 +0800

    原型及继承补充

---
+++ 原型链及继承/实现继承.js
@@ -0,0 +1,83 @@
+//ES5 组合继承
+
+//JS本身是没有继承这一概念的，可以使用原型链实现继承.
+
+  function Person(age){
+    this.age = age
+  }
+  Person.prototype.run = function(){console.log('会跑')}
+  function Man(age,name){
+    Person.call(this,age)
+    this.name = name
+  }
+  function temp(){}
+  temp.prototype = Person.prototype
+  Man.prototype = new temp()
+  Man.prototype.eat = function(){console.log('会吃')}
+  var man = new Man(18,'lili')
+  console.log(man)
+  
+/*
+  优点：在于构造函数可以传参，不会与父类引用属性共享，可以复用父类的函数
+  缺点：调用了父类构造函数，导致子类上多了很多不需要的属性，造成内存浪费。
+ */
+
+// ES5寄生虫继承
+
+function Person(name){
+  this.name = name;
+}
+Person.prototype.run = function(){console.log('会跑')}
+function Man(name,age){
+  Person.call(this,name)
+  this.age = age
+}
+Man.prototype = Object.create(Person.prototype,{
+  constructor: {
+        value: Man,
+        enumerable: true,
+        writable: true,
+        configurable: true
+    }
+})
+var man = new Man('lili',28)
+man.run()
+/*
+将父类的原型赋值给子类，并且将构造函数赋值给子类，这样既解决了无用的父类属性问题，又能正确找到子类的子类的构造函数。
+ */
+
+//ES6
+
+
+  class Person{
+    constructor(age){
+      this.age = age
+    }
+    run(){
+      console.log('会跑')
+    }
+  }
+  class Man extends Person{
+    constructor(age,name){
+      super(age)
+      this.name = name
+    }
+    eat(){
+      console.log('会吃')
+    }
+  }
+  var man = new Man(18,'lili')
+  console.log(man)
+
+
+
+// 使用new关键字 可以实现继承
+
+// 每个实例对象f1，都有一个__proto__属性，指向他的构造函数f的prototype
+
+var f = function(){}
+f.prototype.sun = '继承'
+var f1 = new f()
+console.log(f1.__proto__ === f.prototype) //true
+console.log(f.prototype)
+console.log(f1.__proto__)
