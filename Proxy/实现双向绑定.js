44aca19af684b3b0edea80f2b95c349848f61a74
lhalou <614124284@qq.com>
2020-08-10 11:18:00 +0800

    实双向绑定

--- Proxy/实现双向绑定.js
+++ Proxy/实现双向绑定.js
@@ -1,9 +1,17 @@
+
 // Reflect是一个内置对象，他提供拦截JS操作的基本方法，他不是一个函数对象，因此不可以作为构造函数
 
 let onWatch = (obj,setBind,getLogger) => {
+  function isObject(target) {
+    return  typeof target === 'object' ||  typeof target === 'function' && target!== null
+  }
+  if(!isObject){
+    // 如果是个对象，则返回被代理后的结果，如果不是则直接返回
+     return target;
+  }
   let handler = {
     get(target,property,receiver){
-      if(typeof target[property] === 'object' ||  typeof target[property] === 'function' && target[property !== null]){
+      if(typeof target[property] === 'object' ||  typeof target[property] === 'function' && target[property] !== null){
         return new Proxy(target,handler)
       }
       getLogger(target,property)
@@ -11,6 +19,12 @@ let onWatch = (obj,setBind,getLogger) => {
       //Reflect.get获取对象身上某个属性的值
     },
     set(target,property,value,receiver){
+      //数组下标的更改会触发一次更新，数组长度的修改也会触发一次更新，未避免，只对私有属性的修改动作触发视图更新。
+      if(Array.isArray(target)){
+        if(!target.hasOwnProperty(property)) {
+          setBind(value,property)
+        }
+      } 
       setBind(value,property)
       return Reflect.set(target,property,receiver)
       //Reflect.set将值分配给属性的函数，返回一个boolean
@@ -20,8 +34,12 @@ let onWatch = (obj,setBind,getLogger) => {
 }
 
 let obj = {
-  a:1
+  a:1,
+  b: {
+    name: 1
+  }
 }
+
 let p = onWatch(obj,
 (v,property) => {
   console.log(`监听到属性${property}改变为${v}`)
@@ -31,5 +49,7 @@ let p = onWatch(obj,
 }
 )
 p.a = 2
-
 p.a
+p.b.name = 3
+p.b.name
+
