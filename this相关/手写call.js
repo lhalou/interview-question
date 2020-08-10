dd0111e82a4413da7ed279272736cde6188f365e
lhalou <614124284@qq.com>
2020-08-10 20:55:29 +0800

    修改this相关

--- this相关/手写call.js
+++ this相关/手写call.js
@@ -1,10 +1,13 @@
 //call()方法使用一个指定的this值和单独给出的一个或多个参数来调用一个函数。
 Function.prototype.myCall = function(thisArg){
-  var thisArg = thisArg || window
-  thisArg.fn = this
-  let args = [...arguments].slice(1)
-  let result = thisArg.fn(...args)
-  delete thisArg.fn
-  return result
+  if(typeof this !== 'function'){
+    throw new Error('error')
+  }
+  var thisArg = thisArg || window // 若么有传入第一个参数，默认为window
+  thisArg.fn = this // 改变this的指向，给thisArg设置一个fn属性，指向需要调用的函数
+  let args = [...arguments].slice(1) // slice分离出原函数的参数
+  let result = thisArg.fn(...args) // 调用原函数
+  delete thisArg.fn //删除对象上的函数属性
+  return result // 返回结果
 }
 
--- this相关/手写new.js
+++ this相关/手写new.js
@@ -15,3 +15,5 @@ var b = myNew(a,1,2)
 console.log(b.hi) //hello
 console.log(b.x)  // 1
 console.log(b.y)  // 2
+
+//建议使用字面量的方式创建对象，因为使用new Object(),需要查找原型链，一层层直到找到Object
