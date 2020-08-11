cbebc1af84097cda3c454949b2f8c571cf429b83
lhalou <614124284@qq.com>
2020-08-11 09:34:20 +0800

    闭包不会造成内存泄漏

--- 数据类型/数据类型判断/手写instanceof.js
+++ 数据类型/数据类型判断/手写instanceof.js
@@ -2,7 +2,7 @@
 function myInstanceof(left,right){
   let prototype = right.prototype //获取类型的原型
   let left = left.__proto__ // 获取对象的原型
-  while(true){ //一直判断对象原型是否等于类型原型，知道原型链的顶端null
+  while(true){ //一直判断对象原型是否等于类型原型，直到原型链的顶端null
     if(left === null || left === undefined){
       return false
     }
